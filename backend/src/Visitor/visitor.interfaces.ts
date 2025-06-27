040484a8d662af6259e0abc31f8e7ab1b9ea001a
naye288 <guevar4.nay3@estudiantec.cr>
2025-06-27 17:10:06 -0600

    arreglado en teoria

--- backend/src/Visitor/visitor.interfaces.ts
+++ backend/src/Visitor/visitor.interfaces.ts
@@ -1,6 +1,8 @@
-import { Document } from 'mongoose';
-import { ISession } from '../models/session.model';
-import { IAgendaItem } from '../models/agenda.model';
+import { Document, InferSchemaType } from 'mongoose';
+import { ISession,AgendaItemSchema } from '../models/session.model';
+
+// Extrae el tipo del esquema AgendaItemSchema
+type AgendaItemType = InferSchemaType<typeof AgendaItemSchema>;
 
 // Interface para elementos visitables
 export interface IVisitableSession {
@@ -10,6 +12,6 @@ export interface IVisitableSession {
 // Interface para el Visitor
 export interface ISessionVisitor {
     visitSession(session: ISession & Document): Promise<void>;
-    visitAgendaItem(item: IAgendaItem): Promise<void>;
+    visitAgendaItem(item: AgendaItemType): Promise<void>;
     getResults(): any;
 }
